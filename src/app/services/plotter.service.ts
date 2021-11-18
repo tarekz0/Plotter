bdd3173f306c253ec9734fd504d746e7f6ce289b
Tarek <tarek.zezo@outlook.com>
2021-11-18 19:34:29 +0200

    handling service error

--- src/app/services/plotter.service.ts
+++ src/app/services/plotter.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from 'src/environments/environment';
+import {Observable, of, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -11,10 +13,22 @@ export class PlotterService {
     }
 
     getPlotterColumns() {
-        return this.http.get(environment.url + 'columns');
+        return this.http.get(environment.url + 'columns').pipe(
+            catchError((err) => {
+                console.log('error caught in service');
+                console.error(err);
+                return throwError(err);
+            })
+        );
     }
 
     getColumnsData(columnsForm) {
-        return this.http.post(environment.url + 'data', columnsForm);
+        return this.http.post(environment.url + 'data', columnsForm).pipe(
+            catchError((err) => {
+                console.log('error caught in service');
+                console.error(err);
+                return throwError(err);
+            })
+        );
     }
 }
